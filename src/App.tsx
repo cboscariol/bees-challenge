06c685f8da086215f94b219836fb076b70b615d0
Camila <caamila.boscariol@gmail.com>
2022-06-04 20:10:29 -0300

    implements router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './styles/global.css'
-import Login from './views/Login';
-import UserProvider from './store/userContext'
+import "./styles/global.css";
+import Login from "./views/Login";
+import UserProvider from "./store/userContext";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <UserProvider>
-      <Login/>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<p>HOME</p>} />
+      </Routes>
     </UserProvider>
   );
 }
